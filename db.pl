f6c33dcdf35fb6f7aa7c8550de20f027ee9b4667
E.Cannon <ekcannon@iastsate.edu>
2014-07-22 07:11:30 -0500

    config file

---
+++ db.pl
@@ -0,0 +1,15 @@
+sub connectToDB {
+  my $connect_str = '';
+  my $user        = '';
+  my $pass        = '';
+
+  # $g_connect_str, $g_user, and $g_pass defined in db.pl
+  my $dbh = DBI->connect($connect_str, $user, $pass);
+
+  $dbh->{AutoCommit} = 0;  # enable transactions, if possible
+  $dbh->{RaiseError} = 1;
+
+  return $dbh;
+}#connectToDB
+
+1;
